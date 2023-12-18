51a37a9abb892b12e181d5c552ece37c753ed769
LDWM <leigh-mitchell@hotmail.co.uk>
2023-12-18 12:53:51 +0000

    added absolute positioning to icons in list

---
+++ Javascript/main.js
@@ -0,0 +1,9 @@
+let nametag = document.getElementById ('name');
+
+
+function slideonhover() {
+    nametag.stle.background = orange;
+};
+
+nametag.addEventListener('mouseover, changeBackground');
+
